fbe14adb3c9d62c58488070322c30e2d3a65d3f9
Sourya Vatsyayan <sourya@deepsource.io>
2019-07-24 16:36:31 +0530

    Add case for issues on last line of a file without...
    ... trailing new line
    
    Signed-off-by: Sourya Vatsyayan <sourya@deepsource.io>

---
+++ sample_without_trailing_newline.py
@@ -0,0 +1,35 @@
+import os
+import random
+import this
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+    
+def some_function():
+    print("this should raise a documentation related issue.")
+
+
+def some_other_function():
+    print("this should raise a documentation related issue.")
+
+    # skipcq
+    # some comment here
+    # another comment
+    raise NotImplemented
+
+
+def return_none():
+    return None
+
+
+def dummy_function():
+    print('foo')
+    val = return_none()  # skipcq: pyl-e1128
+
+
+def new_dummy_function():
+    print('foobar')
+    raise NotImplemented  # raise an issue here
